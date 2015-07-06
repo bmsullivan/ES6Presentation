352a2770a773a775d93f5856f8f8b4121ec09587
Brian Sullivan <bmsullivan@gmail.com>
2015-07-06 08:05:14 -0500

    Add specs for const and destructuring

--- dist/variableSpecs.js
+++ dist/variableSpecs.js
@@ -3,7 +3,44 @@
 describe('Variable Specs', function () {
 
     it('should use "let"', function () {
-        for (var _i = 0; _i < 5; _i++) {}
-        expect(i).toBe(5);
+        for (var i = 0; i < 5; i++) {}
+        //expect(i).toBe(5);
+    });
+
+    it('should use constants', function () {
+        var PI = 3.14159;
+        //PI = 3;
+        expect(PI).toBe(3.14159);
+    });
+
+    it('should destructure an array', function () {
+        var a = [1, 2];
+        var b = a[0];
+        var c = a[1];
+
+        expect(b).toBe(1);
+        expect(c).toBe(2);
+    });
+
+    it('should destructure objects', function () {
+        var a = { b: 1, c: 2 };
+
+        var x = a.b;
+        var y = a.c;
+
+        expect(x).toBe(1);
+        expect(y).toBe(2);
+    });
+
+    it('should destructure parameters', function () {
+        var settings = { url: 'foo', method: 'POST' };
+        doSomething(settings);
+        function doSomething(_ref) {
+            var url = _ref.url;
+            var method = _ref.method;
+
+            expect(url).toBe('foo');
+            expect(method).toBe('POST');
+        }
     });
 });
--- specs/variableSpecs.js
+++ specs/variableSpecs.js
@@ -1,8 +1,40 @@
 describe('Variable Specs', function() {
 
-    xit('should use "let"', function() {
+    it('should use "let"', function() {
         for(let i = 0; i < 5; i++){}
-        expect(i).toBe(5);
+        //expect(i).toBe(5);
     });
 
+    it('should use constants', function () {
+        const PI = 3.14159;
+        //PI = 3;
+        expect(PI).toBe(3.14159);
+    });
+
+    it('should destructure an array', function() {
+        let a = [1, 2];
+        let [b, c] = a;
+
+        expect(b).toBe(1);
+        expect(c).toBe(2);
+    });
+
+    it('should destructure objects', function () {
+        let a = { b: 1, c: 2 };
+
+        let { b: x, c: y } = a;
+
+        expect(x).toBe(1);
+        expect(y).toBe(2);
+
+    });
+
+    it('should destructure parameters', function () {
+        let settings = { url: 'foo', method: 'POST' };
+        doSomething(settings);
+        function doSomething({url, method}) {
+            expect(url).toBe('foo');
+            expect(method).toBe('POST');
+        }
+    });
 });
