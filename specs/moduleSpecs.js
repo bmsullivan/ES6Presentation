1c2865bda327cda834b2cae20f7b3d7d5ba106d5
Brian Sullivan <bmsullivan@gmail.com>
2015-07-18 17:13:38 -0500

    Add first module spec

---
+++ specs/moduleSpecs.js
@@ -0,0 +1,10 @@
+import { Person } from '../src/person';
+describe('Module specs', function() {
+    it('should allow imported classes to be used', function() {
+
+        expect(Person).toBeDefined();
+
+        let p = new Person('Brian');
+        expect(p.name).toBe('Brian');
+    });
+});
