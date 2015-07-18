11b0c11d6b569291b0255011af1f060f50d1659a
Brian Sullivan <bmsullivan@gmail.com>
2015-07-18 17:58:19 -0500

    Show use of Parent with Person explicitly imported

--- specs/moduleSpecs.js
+++ specs/moduleSpecs.js
@@ -1,18 +1,18 @@
-import Person from '../src/person';
+//import Person from '../src/person';
 import Parent from '../src/parent';
 describe('Module specs', function() {
-    it('should allow imported classes to be used', function() {
-
-        expect(Person).toBeDefined();
-
-        let p = new Person('Brian');
-        expect(p.name).toBe('Brian');
-    });
+    //it('should allow imported classes to be used', function() {
+    //
+    //    expect(Person).toBeDefined();
+    //
+    //    let p = new Person('Brian');
+    //    expect(p.name).toBe('Brian');
+    //});
 
     it('should chain imports', function() {
-        let child = new Person('Molly');
-        let p = new Parent('Brian', [child]);
+        //let child = new Person('Molly');
+        let p = new Parent('Brian', []);
 
-        expect(p.children[0].name).toBe('Molly');
+        expect(p.children.length).toBe(0);
     });
 });
