6139ea180458893ba3fe0a5db1577d506a2be7c9
Brian Sullivan <bmsullivan@gmail.com>
2015-12-01 21:20:59 -0600

    Fix babelify build

--- gulpfile.js
+++ gulpfile.js
@@ -2,27 +2,18 @@ var gulp = require('gulp');
 var browserify = require('browserify');
 var babelify = require('babelify');
 var source = require('vinyl-source-stream');
-var through = require('through2');
-var globby = require('globby');
+var glob = require('glob');
 
 gulp.task("scripts", function () {
-    var bundledStream = through();
-
-    bundledStream
+    var files = glob.sync('./specs/*.js');
+    browserify({
+        entries: files,
+        transform: [babelify.configure()]
+    })
+        .bundle()
         .pipe(source('output.js'))
         .pipe(gulp.dest('./dist/'));
 
-    globby(['./specs/*.js'], function (err, entries) {
-        var b = browserify({
-            entries: entries,
-            debug: true,
-            transform: [babelify.configure()]
-        });
-
-        b.bundle().pipe(bundledStream);
-    });
-
-    return bundledStream;
 });
 
 gulp.task('watch', function () {
